5f44ec612c8f50469a22f6234e5f58d1387b32da
Athene <sillycatathene@gmail.com>
2024-12-02 09:54:56 +0100

    CSS Challenges

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const expandButton = document.querySelector('body section button');
+expandButton.addEventListener('click', hidee); 
+
+function hidee() {
+  var div1 = document.getElementsByClassName("inhoud").setAttribute(class, hidden); 
+}
